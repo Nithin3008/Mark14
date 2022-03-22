8fa559c2ef9cb138cc8ed7664c6f0aefed393b15
Nithin3008 <60174062+Nithin3008@users.noreply.github.com>
2022-03-22 21:09:30 +0530

    add css styles

---
+++ app.js
@@ -0,0 +1,42 @@
+var intialPrice=document.querySelector("#intialPrice")
+var quantity=document.querySelector("#quantity")
+var sellingPrice=document.querySelector("#currentPrice")
+var btn=document.querySelector("#btn")
+function profit(ip,sp,q)
+{
+   var sp1=sp*q;
+    var ip1=ip*q;
+    var profit1=sp1-ip1;
+    var profitPer=(profit1/ip1)*100;
+    msg.innerText="Profit you received "+profit1+" profit percentage "+profitPer+"%"
+}
+function loss(ip,sp,q)
+{
+    var sp1=sp*q;
+    var ip1=ip*q;
+    var loss1=ip1-sp1
+    var lossPer=(loss1/ip1)*100
+    msg.innerText="Loss occured "+loss1+" loss percentage "+lossPer+"%"
+    
+}
+
+function calculateProfitAndLoss()
+{
+    var ip=Number(intialPrice.value)//added number here even input type text because below 100 values are not comparing wellit was going into profit function and returning -ve values
+    var q=Number(quantity.value);
+    var sp=Number(sellingPrice.value)
+    
+    if(ip>sp)
+    {
+        loss(ip,sp,q)
+        console.log("loss")
+    }
+    else  if(sp>ip)
+    {
+        console.log("profit")
+        profit(ip,sp,q)
+        
+    }
+}
+
+btn.addEventListener("click",calculateProfitAndLoss)
